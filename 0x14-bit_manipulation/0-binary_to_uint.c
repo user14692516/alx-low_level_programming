fba8fc071bc93baae2d75f838fb850093a1a5665
user14692516 <kipkuruidenniskiptoo@gmail.com>
2023-04-06 10:02:17 -0700

    Initial commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,26 +8,16 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len,base_two;
+	int i;
+	unsigned int dec_val = 0;
 
-	if(!b)
-		return(0);
-	ui = 0;
-
-	for (len = 0;b[len] !='\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *=2)
-	{
-		if(b[len] != '0' && b[len] ! = '1')
-		{
+		if(!b)
 			return(0);
-		}
-		if(b[len] & 1)
-		{
-			ui += base_two;
-		}
+	for (i=0;b[i];i++)
+	{
+		if (b[i]<'0'||b[i]>'1')
+			return(0);
+		dec_val = 2*dec_val +(b[i]-'0');
 	}
-	return(ui);
+	return(dec_val);
 }
