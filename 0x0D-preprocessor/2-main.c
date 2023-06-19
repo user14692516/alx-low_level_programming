bc9f2566c74336c47740c5d08ad5ced7d2298205
user14692516 <kipkuruidenniskiptoo@gmail.com>
2023-06-19 09:56:04 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the file name in which it was compiled from
+ *
+ * Return : 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef LIKEMACRO
+#define LIKEMACRO
+
+#define ABS(x) ((x) < (0) ? -(x) : (x))
+
+#endif
